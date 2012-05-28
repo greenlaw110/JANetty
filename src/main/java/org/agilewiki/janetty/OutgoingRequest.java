b606d2b153b80e8c0e21adcf6673253b10119309
Bill LaForge <laforge49@gmail.com>
2012-05-28 10:41:23 +0530

    Simplification: there is only one Protocol1 instance per port.

--- src/main/java/org/agilewiki/janetty/OutgoingRequest.java
+++ src/main/java/org/agilewiki/janetty/OutgoingRequest.java
@@ -5,29 +5,12 @@ import org.agilewiki.jactor.lpc.Request;
 
 public class OutgoingRequest extends Request<Object, Com> {
 
-    public final String sourceProtocolName;
-    public final String destinationProtocolName;
-    public final String destinationProtocolFactoryName;
     public final byte[] payload;
 
     /**
      * Sent to the actor on the remote system with the given destinationProtocolName.
      */
-    public OutgoingRequest(String sourceProtocolName, String destinationProtocolName, byte[] payload) {
-        this.sourceProtocolName = sourceProtocolName;
-        this.destinationProtocolName = destinationProtocolName;
-        this.destinationProtocolFactoryName = null;
-        this.payload = payload;
-    }
-
-    /**
-     * Sent to the actor on the remote system with the given destinationProtocolName, creating it as needed.
-     */
-    public OutgoingRequest(String sourceProtocolName, String destinationProtocolName,
-                           String destinationProtocolFactoryName, byte[] payload) {
-        this.sourceProtocolName = sourceProtocolName;
-        this.destinationProtocolName = destinationProtocolName;
-        this.destinationProtocolFactoryName = destinationProtocolFactoryName;
+    public OutgoingRequest(byte[] payload) {
         this.payload = payload;
     }
 
